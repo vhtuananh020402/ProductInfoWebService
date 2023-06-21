b0784236f3452d4f79b3c63e9b48a0a8b7f0a8d7
Tuna <vhtuananh020402@gmail.com>
2023-06-22 06:25:45 +0700

    remove redundancy

--- app.js
+++ app.js
@@ -28,27 +28,6 @@ async function render(qrCode) {
 }
 
 
-
-// async function renderUsers() {
-//     let users = await getUsers();
-//     let html = '';
-//     users.forEach(user => {
-//         let htmlSegment = `<div class="user">
-//                             <img src="${user.unicode}" >
-//                             <h2>${user.firstName} ${user.lastName}</h2>
-//                             <div class="email"><a href="email:${user.email}">${user.email}</a></div>
-//                         </div>`;
-
-//         html += htmlSegment;
-//     });
-
-//     let container = document.querySelector('.container');
-//     container.innerHTML = html;
-// }
-
-// renderUsers();
-
-
 function onScanSuccess(qrCodeMessage) {
     // document.getElementById('result').innerHTML = '<span class="result">'+qrCodeMessage+'</span>';
     render(qrCodeMessage);
